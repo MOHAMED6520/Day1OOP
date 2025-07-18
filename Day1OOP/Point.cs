5c488d6ec7e834969c7f9b34d2ff4c5c784fffc4
moham <moham@DESKTOP-M8CT1H1>
2025-07-19 01:01:20 +0300

    Struct

---
+++ Day1OOP/Point.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1OOP
+{
+    internal struct Point
+    {
+        //public int X = default; 
+        //public int Y = default;
+        ////public Point()
+        ////{
+        ////}
+        ////public Point()
+        ////{
+        ////    X =8; Y =8;
+        ////}
+        //public Point(int x,int y)
+        //{
+        //    X = x;
+        //    Y = y;
+        //}
+
+        //public void PrintPoint()
+        //{
+        //    Console.WriteLine($"({X},{Y})");
+        //}
+
+        //public override string ToString()
+        //{
+        //    return $"({X} , {Y})";
+        //}
+    }
+}
--- Day1OOP/Program.cs
+++ Day1OOP/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Struct
+            ////Struct
+            //Point P01;
+            ////P01.X = 10;
+            ////P01.Y = 10;
+            ////Console.WriteLine(P01.X);
+            ////Console.WriteLine(P01.Y);
+
+            //P01=new Point();
+            ////Console.WriteLine(P01.X);
+            ////Console.WriteLine(P01.Y);
+            //P01 = new Point(14, 15);
+            ////Console.WriteLine(P01.X);
+            ////Console.WriteLine(P01.Y);
+
+            //// Console.WriteLine(P01);
+            ////  P01.PrintPoint();
+            //Console.WriteLine(P01.ToString()); 
+            #endregion
         }
     }
 }
