4a654e22b93a73e22c598b6bdddaa4b549ee144a
moham <moham@DESKTOP-M8CT1H1>
2025-07-19 07:44:50 +0300

    Q01

---
+++ Day1OOP/Point.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1OOP
+{
+    internal struct Point
+    {
+		private int x;
+
+		private int y;
+
+        public int X
+        {
+
+            set
+            {
+                 if (value >= 0)
+                x = value;
+            }
+
+            get { return x; }
+
+
+          
+        }
+        public int Y
+		{
+			get { return y; }
+
+			set {  
+				if (value >= 0) 
+				y = value; 
+			    }
+		}
+         public void PrintDistance ()
+        {
+            if (this.X > this.Y)
+                Console.WriteLine($"The Distanse Between Two Point : {this.X - this.Y}");
+
+            else
+                Console.WriteLine($"The Distanse Between Two Point : {this.Y - this.X}");
+
+        }
+
+
+
+
+    }
+}
--- Day1OOP/Program.cs
+++ Day1OOP/Program.cs
@@ -4,7 +4,29 @@
     {
         static void Main(string[] args)
         {
-            
+            #region Q01
+            //Point P01 = new Point();
+            //int x, y;
+            //bool flag;
+            //do
+            //{
+            //    Console.Write("Enter X :");
+            //    flag= int.TryParse(Console.ReadLine(), out x);
+            //}
+            //while (flag==false||x<0);
+            //P01.X = x;
+
+            //do
+            //{
+            //    Console.Write("Enter Y :");
+            //   flag= int.TryParse(Console.ReadLine(), out y);
+            //}
+            //while (flag == false || y<0);
+            //P01.Y = y;
+
+            //P01.PrintDistance(); 
+            #endregion
+
         }
     }
 }
